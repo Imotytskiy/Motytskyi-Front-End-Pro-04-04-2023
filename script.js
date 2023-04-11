980d8e3689fcbf03f33bfaf81762105cbf8c6cfc
Imotytskiy <Imotytskiy@gmail.com>
2023-04-11 15:46:14 +0200

    add files

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
 "use strict";
-//first level
-// function secCount(){
-
-//   let hourStr = prompt ("Вкажіть кількість годин",'');
-//       hourStr = hourStr.replace(/,/g, ".");
-//       if (/^\d+(\.\d+)?$/.test(hourStr)) {
-//       const hoursNum = Number(hourStr);
-//       const sec = hoursNum*3600;
-//       const arr = [sec,hoursNum];
-//       return arr;
-//     }else{
-//       alert(`Вкажіть кількість годин: 4, 5.5`)
-//       secCount();
-//     }}
-// let seconds = secCount();
-// alert(`${seconds[1]} годин складає ${seconds[0]} секунд`);
-//second level
 function secCount() {
-  let hourStr = document.getElementById("hours").value;
+  let year = document.getElementById("year").value;
+  let city = document.getElementById("city").value;
+  let sport = document.getElementById("sport").value;
   hourStr = hourStr.replace(/,/g, ".");                     // можна прибрати валідація на index.html проходить там можна також зробити патерн
   if (/^\d+(\.\d+)?$/.test(hourStr)) {  
     const hoursNum = Number(hourStr);
