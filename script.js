67312b7293f269435f038ef17951a897b3d64f1b
Imotytskiy <Imotytskiy@gmail.com>
2023-04-11 12:48:19 +0200

    last version

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
+
+// Переписати код нижче з використанням конструкції switch…case
+
+// let numOrStr = prompt('input number or string');
+// console.log(numOrStr)
+
 "use strict";
-   // First level
+   
+let numOrStr = +prompt('input number or string');
 
-// let sum = 0;
-
-// for (let i = 1; i <= 3; ++i) {
-//    sum = sum + Number(prompt(`Введіть число ${i}`,""));
-// };
-// alert((sum/3).toFixed(2));
-
-   // Second level
-
-const numRegex = /^-?\d*\.?\d+(e[-+]?\d+)?$/; 
-let num, sum = 0;
-for (let i = 1; i <= 3; ++i) {
-   num = Number(prompt(`Введіть число ${i}`,"").replace(/,/g, "."))
-   if (numRegex.test(num)) {
-      sum += num;
-   } else {
-      alert("Тільки числа:");
-      i--;
-   }
-};
-alert(`Середнє арифметичне ${(sum/3).toFixed(2)}`);
+   switch (isNaN(numOrStr)) {
+      case true:
+          console.log("string");
+          break;
+      case false:
+          console.log("number");
+          break;
+      default:
+         console.log("unknown type");
+         break;
+    }
+   
