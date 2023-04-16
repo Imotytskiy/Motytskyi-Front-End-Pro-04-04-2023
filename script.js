bb14fee4e3e8399264562b8270806fdb642f40b6
Imotytskiy <Imotytskiy@gmail.com>
2023-04-16 22:32:16 +0200

    add second variant

--- script.js
+++ script.js
@@ -6,3 +6,33 @@
 // console.log(array);
 // // Результат: [1, 2, 3, 4, 6, 7]Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
 
+//доповнення видаляємо елемент (5)
+// Перший варіант 
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+function removeElement(array, arg = 5) {
+  let indexDel = undefined; 
+  array.forEach((element, index) => {
+    if (element === arg) {
+      indexDel = index;
+    }
+  });
+  array.splice(indexDel, 1);
+  return array;
+}
+console.log(removeElement(array));
+
+
+// Другий варіант
+
+// const array = [1, 2, 3, 4, 5, 6, 7];
+
+// function removeElement(array, arg = 5) {
+//   var evenArray = array.filter(function(element) {
+//     return element !== arg;
+//   });
+//   return evenArray;
+// }
+
+// console.log(removeElement(array));
