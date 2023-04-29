820e9e59650194100e87ebf6254bf73495ae1c2b
Imotytskiy <Imotytskiy@gmail.com>
2023-04-29 20:15:04 +0200

    add

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+
+    //  ВАРИАНТ 1 
+
+// let step = 0; // делаем глобальной window.step
+
+// let ladder = {
+//     up: function () {
+//        step++;
+//        return this;
+//     },
+//     down: function () {
+//        step--;
+//        return this;
+//     },
+//     showStep: function () {
+//         alert(step);
+//     }
+// };
+// ladder.up().up().down().showStep();
+
+let ladder = {
+    step: 0,
+    up: function () {
+        // ladder.step++;
+        this.step++;    
+        return this;
+    },
+    down: function () {
+        // ladder.step--;
+        this.step--;
+        return this;
+        
+    },
+    showStep: function () {
+        alert(this.step);
+        // return this;
+    }
+};
+ladder.up().up().down().showStep();
+
+
 // let ladder = {
 //     step: 0,
 //     up: function () {
@@ -24,23 +65,3 @@
 // }
 
 // useAllSteps(ladder);   // буквальное решении ladder.up().up().down().showStep(); // 1
-
-
-let ladder = {
-    step: 0,
-    up: function () {
-        this.step++;
-        return this;
-    },
-    down: function () {
-        this.step--;
-        return this;
-        
-    },
-    showStep: function () {
-        alert(this.step);
-        // return this;
-    }
-};
-
-ladder.up().up().down().showStep();
