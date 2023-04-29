6b9404c14730c28d3cbd6d09a78c8b3cd74eed5f
Imotytskiy <Imotytskiy@gmail.com>
2023-04-29 19:40:17 +0200

    add new  version

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
+// let ladder = {
+//     step: 0,
+//     up: function () {
+//         this.step++;
+//     },
+//     down: function () {
+//         this.step--;
+//     },
+//     showStep: function () { // показывает текущую ступеньку
+//         alert(this.step);
+//     }
+    
+// };
+
+// const useAllSteps = (obj) => {
+//     for(const key in obj){
+//         if (key === "up") { 
+//             obj[key]();
+//         }
+//         if (typeof obj[key] === "function") {
+//             obj[key]();
+//         }
+//     }
+// }
+
+// useAllSteps(ladder);   // буквальное решении ladder.up().up().down().showStep(); // 1
+
+
 let ladder = {
     step: 0,
     up: function () {
         this.step++;
+        return this;
     },
     down: function () {
         this.step--;
+        return this;
+        
     },
-    showStep: function () { // показывает текущую ступеньку
+    showStep: function () {
         alert(this.step);
+        // return this;
     }
-    
 };
 
-const useAllSteps = (obj) => {
-    for(const key in obj){
-        if (key === "up") { 
-            obj[key]();
-        }
-        if (typeof obj[key] === "function") {
-            obj[key]();
-        }
-    }
-}
-
-useAllSteps(ladder);   // буквальное решении ladder.up().up().down().showStep(); // 1
+ladder.up().up().down().showStep();
