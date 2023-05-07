a2c2ef4c05bf72aa66a8c4e93c00fd424a788405
Imotytskiy <Imotytskiy@gmail.com>
2023-05-07 18:38:04 +0200

    add obj

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
 
-// Вам потрібно зробити конструктор сутності "Студент".
-
-
-
-// Студент має ім'я, прізвище, рік народження — це властивості. Є масив з оцінками, це також властивість. І є можливість отримати вік студента та його середній бал – це методи.
-
-
-
-// Ще у всіх Студентів є по масиву однакової довжини, у ньому 25 елементів, спочатку він не заповнений, але на 25 елементів. Це масив, в якому відзначається відвідуваність, щоразу коли ми викликаємо метод .present() на чергове порожнє місце, в масив записується true, коли викликаємо .absent() - записується false. Передбачте будь-який захист від того, щоб у масиві відвідуваності не могло бути більше 25 записів. Масив – це властивість, present та absent – ​​методи.
-
-
-
-// Останній метод: .summary(), перевіряє середню оцінку і середнє відвідування(кількістьВідвідин/кількістьЗанять), і якщо середня оцінка більше 90, а середнє відвідування більше 0.9, то метод summary повертає рядок "Молодець!", якщо одне з цих значень менше , то - "Добре, але можна краще ", якщо обидва нижче - "Редиска!".
-
-
-
-// Не забудьте після того, як напишите цей конструктор, створити 2-3 екземпляри (конкретних студентів) і показати використання цих методів.
+const now = new Date();
+const currentYear = now.getFullYear();
 
 class Student {
     constructor(identity){
         this.name = identity.name;
         this.surname = identity.surname;
         this.birthYear = identity.birthYear;
-        this.arrScore = new Array();    
+        this.arrScore = new Array(25);                    // масив з оцінками, це також властивість, є масив 25 довжиною
+        this.bornYear = this.determinateAge(); 
+        this.everageScore = this.summary();
     }
 
+
+    determinateAge() {                                    // вік студента метод
+        return currentYear - this.birthYear;
+    }
+
+    present(){
+        const index = this.arrScore.findIndex((element) => element === undefined); 
+        if (index !== -1) {
+            this.arrScore[index]= true;
+        // } else {
+        //     console.log("Використані всі 25");
+        }
+
+    }
+
+    absent(){
+        const index = this.arrScore.findIndex((element) => element === undefined); 
+        if (index !== -1) {
+            this.arrScore[index] = false;
+        // } else {
+        //     console.log("Використані всі 25");
+        }
+    }
+
+        summary() {                                                            // середній бал метод  90% від 25 дорівнює 22.5 округлюємо
+            const sum = this.arrScore.reduce((acc, curr) => acc + curr);
+            switch(true) {
+              case (Math.round((sum / this.arrScore.length) * 25) <= 0.9 ):
+                return "Молодець";
+              case (Math.round((sum / this.arrScore.length) * 25) > 0.9 ):
+                return "Добре, але можна краще ";
+              default:
+                return "Error";
+            }
+      }
 }
+
+const firstStudent = new Student({
+    name : "Adam",
+    surname : "Smith",
+    birthYear : 1985
+});
