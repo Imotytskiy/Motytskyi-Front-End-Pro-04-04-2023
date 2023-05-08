44421cdfbd177b33f3394bdf7dbb69875ff18073
Imotytskiy <Imotytskiy@gmail.com>
2023-05-08 13:26:41 +0200

    add arr score

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ class Student {
         this.name = identity.name;
         this.surname = identity.surname;
         this.birthYear = identity.birthYear;
-        this.arrScore = new Array(25);
+        this.arrScore = identity.arrScore;                       // масив з оцінками, це також властивість
+        this.arrVisit = new Array(25);                          // у всіх Студентів є по масиву однакової довжини, у ньому 25 елементів                                  
         this.bornYear = this.determinateAge(); 
         this.everageScore = this.summary();
     }
@@ -16,44 +17,58 @@ class Student {
     }
 
     present(){
-        const index = this.arrScore.findIndex((element) => element === undefined); 
+        const index = this.arrVisit.findIndex((element) => element === undefined); 
         if (index !== -1) {
-            this.arrScore[index] = true;
+            this.arrVisit[index] = true;
         }
         return this;
     }
 
     absent(){
-        const index = this.arrScore.findIndex((element) => element === undefined); 
+        const index = this.arrVisit.findIndex((element) => element === undefined); 
         if (index !== -1) {
-            this.arrScore[index] = false;
+            this.arrVisit[index] = false;
         }
         return this;
     }
 
     summary() {
-        const sum = this.arrScore.reduce((acc, curr) => acc + curr);
-        const avg = sum / this.arrScore.length;
+        const sum = this.arrVisit.reduce((acc, curr) => acc + (curr ? 1 : 0),0);   // undefined не рахую 
+        const avg = sum / this.arrVisit.length;
 
-        if (avg >= 0.9) {
+        if (avg >= 0.9 && avgScore >= 90) {                       
             return "Молодець";
-        } else {
+        }else if (avg >= 0.9 && avgScore < 90 || avg < 0.9 && avgScore > 90) {
             return "Добре, але можна краще";
+        }else{
+            return "Редиска";
         }
     }
 }
 
+
+
 const firstStudent = new Student({
     name : "Adam",
     surname : "Smith",
-    birthYear : 1985
+    birthYear : 1985,
+    arrScore : [90,91,92,90,95]
 });
 
 const secondStudent = new Student({
     name : "Eva",
     surname : "Smith",
-    birthYear : 1995
+    birthYear : 1995,
+    arrScore : [80,70,60,89,50]
 });
+
+const thirdStudent = new Student({
+    name : "Eva",
+    surname : "Smith",
+    birthYear : 1995,
+    arrScore : [80,70,60,89,50]
+});
+
 
 firstStudent
 .absent().absent().absent().absent().absent()
@@ -62,15 +77,17 @@ firstStudent
 .present().present().present().present().present()
 .present().present().present().present().present();
 
-console.log(firstStudent.everageScore);
+console.log(firstStudent.summary());
 console.log(firstStudent.bornYear);
 
-firstStudent
+secondStudent
 .absent().present().present().present().present()
 .present().present().present().present().present()
 .present().present().present().present().present()
 .present().present().present().present().present()
 .present().present().present().present().present();
 
-console.log(firstStudent.everageScore);
+console.log(firstStudent.summary());
 console.log(firstStudent.bornYear);
+
+thirdStudent
