6e01f40aa7c50f05ab2381231fb26d2d59ccc41a
Imotytskiy <Imotytskiy@gmail.com>
2023-05-16 13:52:04 +0200

    add design

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const parentElement = document.querySelector('body');
-parentElement.style.backgroundColor = 'black';
+parentElement.style.backgroundColor = '#89CFF0';
+parentElement.style.marginTop = '10%';
+parentElement.style.display = 'flex';
+parentElement.style.justifyContent = 'center';
+parentElement.style.alignItems = 'center';
 
-const table = document.createElement('table');
-parentElement.appendChild(table);
-table.style.marginTop = '5%';
-table.style.display = 'flex';
-table.style.justifyContent = 'center';
-table.style.alignItems = 'center';
+function makeColumnsRows(Columns = 10, Rows = 10) {
+  const table = document.createElement('table');
+  parentElement.appendChild(table);
+
+    for (let i = 0; i < Rows; i++) {
+        const tr = document.createElement('tr');
+        table.appendChild(tr);
+        for (let j = 0; j < Columns; j++) {
+            const td = document.createElement('td');
+            tr.appendChild(td);
+            td.style.color = 'white';
+            td.style.fontSize = '16';
+            td.textContent = 'CELL';
+        }
+    }
+}
+
+makeColumnsRows();
