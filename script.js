c24b78ca4fe9ee9f195d541da4e2505da6d54b5d
Imotytskiy <Imotytskiy@gmail.com>
2023-05-16 11:44:54 +0200

    refactor only functions

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 
+const parentElement = document.querySelector('body');
+parentElement.style.backgroundColor = 'black';
+
+const firstDiv = document.createElement('div');
+parentElement.appendChild(firstDiv);
+firstDiv.style.marginTop = '8%';
+firstDiv.style.display = 'flex';
+firstDiv.style.justifyContent = 'center';
+firstDiv.style.alignItems = 'center';
+
+
 function getRandomInt(max = 9) {              //  8 because include 0.jpg (real 9) Expected output: max
   return Math.floor(Math.random() * max) + 1; // exclude 0.jpg use +1
 }
@@ -9,22 +20,15 @@ function takeRandomImg() {
   return pathToTheFile;
 }
 
-
 const randomImagePath = takeRandomImg();
 
-const firstDiv = document.createElement('div');
-firstDiv.style.marginTop = '8%';
-firstDiv.style.display = 'flex';
-firstDiv.style.justifyContent = 'center';
-firstDiv.style.alignItems = 'center';
-
-const parentElement = document.querySelector('body');
-parentElement.style.backgroundColor = 'black';
-parentElement.appendChild(firstDiv);
+function displayImg() {
 
 const newImg = document.createElement('img');
-
 newImg.setAttribute('src', randomImagePath);
 firstDiv.appendChild(newImg);
 
+}
+
+displayImg();
 
