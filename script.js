4b25dae81e51fc1e48058d54e91ca3c337e996e3
Imotytskiy <Imotytskiy@gmail.com>
2023-05-16 12:52:34 +0200

    add table element into DOM

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-  
+const parentElement = document.querySelector('body');
+parentElement.style.backgroundColor = 'black';
+
+const table = document.createElement('table');
+parentElement.appendChild(table);
+table.style.marginTop = '5%';
+table.style.display = 'flex';
+table.style.justifyContent = 'center';
+table.style.alignItems = 'center';
