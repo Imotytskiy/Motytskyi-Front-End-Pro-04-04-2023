9a411a92664a518a5196e60d6f152a1ff02b6b6f
Imotytskiy <Imotytskiy@gmail.com>
2023-05-19 22:49:52 +0200

    add console.log

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-// const parentElement = document.querySelector('body');
-// parentElement.style.backgroundColor = '#89CFF0';
-// parentElement.style.marginTop = '10%';
-// parentElement.style.display = 'flex';
-// parentElement.style.justifyContent = 'space-around';
-// parentElement.style.alignItems = 'center';
+const parentElement = document.querySelector('body');
+parentElement.style.backgroundColor = '#89CFF0';
+parentElement.style.marginTop = '10%';
+parentElement.style.display = 'flex';
+parentElement.style.justifyContent = 'space-around';
+parentElement.style.alignItems = 'center';
 
-// const firstButton = document.createElement('button');
-// firstButton.textContent = 'Реалізувати перевірку на http/https';
-// firstButton.setAttribute('type', 'submit');
-// parentElement.appendChild(firstButton);
+const firstButton = document.createElement('button');
+firstButton.textContent = 'Реалізувати перевірку на http/https';
+firstButton.setAttribute('type', 'submit');
+parentElement.appendChild(firstButton);
 
-// const inputElement = document.createElement('input');
-// inputElement.setAttribute('type', 'url');
-// inputElement.setAttribute('name', 'url');
-// inputElement.setAttribute('id', 'url');
-// inputElement.setAttribute('placeholder', 'www.example.com');
-// inputElement.setAttribute('size', '30');
-// inputElement.setAttribute('required', '');
-// parentElement.appendChild(inputElement);
+const inputElement = document.createElement('input');
+inputElement.setAttribute('type', 'url');
+inputElement.setAttribute('name', 'url');
+inputElement.setAttribute('id', 'url');
+inputElement.setAttribute('placeholder', 'www.example.com');
+inputElement.setAttribute('size', '30');
+inputElement.setAttribute('required', '');
+parentElement.appendChild(inputElement);
 
-// const secondButton = document.createElement('button');
-// secondButton.textContent = 'Переадресовується на інший сайт';
-// secondButton.setAttribute('type', 'button');
-// parentElement.appendChild(secondButton);
+const secondButton = document.createElement('button');
+secondButton.textContent = 'Переадресовується на інший сайт';
+secondButton.setAttribute('type', 'button');
+parentElement.appendChild(secondButton);
 
+let url = "";
 
 function validRedirect(urlFromInput) {
-  // let urlSecure = "https://";
-  let url = "";
   if (!urlFromInput.startsWith('http://') && !urlFromInput.startsWith('https://')) {
     url = "http://" + urlFromInput;
   } else {
@@ -36,10 +35,8 @@ function validRedirect(urlFromInput) {
   return url;
 }
 
-// firstButton.addEventListener('click', function () {
-//     const urlFromInput = inputElement.value;
-//     validRedirect(urlFromInput);
-// });
-
-console.log(validRedirect("www.example.com"));
-
+firstButton.addEventListener('click', function () {
+    const urlFromInput = inputElement.value;
+    validRedirect(urlFromInput);
+    console.log(url);
+});
