4cfff20bd06f7a6e2eb8641d1b2c4714a673ccad
Imotytskiy <Imotytskiy@gmail.com>
2023-05-19 15:23:40 +0200

    delete block and none

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-
 const parentElement = document.querySelector('body');
-parentElement.style.backgroundColor = 'black';
+parentElement.style.backgroundColor = 'indigo';
 
 const firstDiv = document.createElement('div');
 parentElement.appendChild(firstDiv);
 firstDiv.style.marginTop = '8%';
 firstDiv.style.display = 'flex';
-firstDiv.style.justifyContent = 'center';
+firstDiv.style.justifyContent = 'space-evenly';
 firstDiv.style.alignItems = 'center';
 
+let currentImageIndex = 4;
 
-function getRandomInt(max = 9) {             
-  return Math.floor(Math.random() * max) + 1; // exclude 0.jpg use +1
-}
-function nextPrev (){
-
-}
-
-
-
-
-
-function nextPrev () {
-  let img = getRandomInt();
-  const pathToTheFile = `images/${img}.jpg`;
-  return pathToTheFile;
-}
-
-
-function displayImg() {
+const prevButton = document.createElement('button');
+prevButton.textContent = 'PREV';
+prevButton.setAttribute('type', 'button');
+firstDiv.appendChild(prevButton);
 
 const newImg = document.createElement('img');
-newImg.setAttribute('src', takeRandomImg());
 newImg.style.width = "400px";
 newImg.style.height = "400px";
 firstDiv.appendChild(newImg);
 
+
+const nextButton = document.createElement('button');
+nextButton.textContent = 'NEXT';
+nextButton.setAttribute('type', 'button');
+firstDiv.appendChild(nextButton);
+
+function displayImg() {
+  newImg.setAttribute('src', `images/${currentImageIndex}.jpg`);
+  prevButton.style.visibility = currentImageIndex <= 1 ? "hidden" : "visible";
+  nextButton.style.visibility = currentImageIndex >= 9 ? "hidden" : "visible";
 }
 
-displayImg();
+prevButton.addEventListener('click', () => {
+  if(currentImageIndex > 1) {
+    currentImageIndex--;
+    displayImg();
+  }
+});
 
+nextButton.addEventListener('click', () => {
+  if(currentImageIndex < 9) {
+    currentImageIndex++;
+    displayImg();
+  }
+});
+
+displayImg();
