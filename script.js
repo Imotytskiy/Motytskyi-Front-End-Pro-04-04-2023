deeec288062839e03c84dd2a0aeb95804c3f460a
Imotytskiy <Imotytskiy@gmail.com>
2023-05-23 22:59:17 +0200

    add kiss

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 import { textTitles, dataShop } from "./data.js";
 
-const getId = document.getElementById("root");
+const appContainer = document.getElementById("root");
+
+const catContainer = appContainer.appendChild(document.createElement("span"));
+const prodContainer = appContainer.appendChild(document.createElement("span"));
+const descrContainer = appContainer.appendChild(document.createElement("span"));
+
+catContainer.textContent = textTitles[0];
+prodContainer.textContent = textTitles[1];
+descrContainer.textContent = textTitles[2];
 
 const createNode = (teg) => {
   return document.createElement(teg);
@@ -24,49 +32,44 @@ const applyStyles = (element) => {
 const allForButton = (element) => {
   element.textContent = textTitles[3];
   element.classList.add("button");
-  getId.children[2].appendChild(element);
+  descrContainer.appendChild(element);
 };
 
 const clearBlock = () => {
-  getId.children[1].innerHTML = textTitles[1];
-  getId.children[2].innerHTML = textTitles[2];
+  prodContainer.innerHTML = textTitles[1];
+  descrContainer.innerHTML = textTitles[2];
 };
-const skeleton = () => {
-  for (let i = 1; i <= 3; i++) {
-    let childBlock = createNode("span");
-    childBlock.textContent = textTitles[i - 1];
-    childBlock.classList.add("span");
-    getId.appendChild(childBlock);
-  }
-};
-skeleton();
 
 dataShop.forEach((category) => {
   const categoryElement = createNode("div");
   applyStyles(categoryElement);
   categoryElement.textContent = category.name;
-  getId.children[0].appendChild(categoryElement);
-  categoryElement.addEventListener("click", function (event) {
-    let initForSecondBlock = category.name;
-    let currentCategory = dataShop.find(
-      (cat) => cat.name === initForSecondBlock
-    );
-    clearBlock();
-
-    currentCategory.products.forEach((product) => {
-      const productElement = createNode("div");
-      applyStyles(productElement);
-      productElement.textContent = product.name;
-      getId.children[1].appendChild(productElement);
-      productElement.addEventListener("click", function (event) {
-        getId.children[2].innerHTML = textTitles[2];
-
-        let descriptionElement = createNode("div");
-        applyStyles(descriptionElement);
-        descriptionElement.textContent = product.description;
-        getId.children[2].appendChild(descriptionElement);
-        createButton();
-      });
-    });
-  });
+  catContainer.appendChild(categoryElement);
+  categoryElement.addEventListener("click", makeCategory);
 });
+
+function makeCategory(event) {
+  let initForSecondBlock = event.target.innerText;
+  let currentCategory = dataShop.find((cat) => cat.name === initForSecondBlock);
+  clearBlock();
+
+  currentCategory.products.forEach((product) => {
+    const productElement = createNode("div");
+    applyStyles(productElement);
+    productElement.textContent = product.name;
+    prodContainer.appendChild(productElement);
+    productElement.addEventListener("click", productSelect(product));
+  });
+}
+
+function productSelect(product) {
+  return function (event) {
+    descrContainer.innerHTML = textTitles[2];
+
+    let descriptionElement = createNode("div");
+    applyStyles(descriptionElement);
+    descriptionElement.textContent = product.description;
+    descrContainer.appendChild(descriptionElement);
+    createButton();
+  };
+}
