d51749481296734465097a4b0446a36543441389
Imotytskiy <Imotytskiy@gmail.com>
2023-05-24 15:47:11 +0200

    add commir not for push

--- script.js
+++ script.js
@@ -2,40 +2,35 @@ const appContainer = document.getElementById("root");
 
 const firstButton = document.createElement("button");
 firstButton.textContent = "Реалізувати перевірку на http/https";
-firstButton.setAttribute("type", "submit");
 appContainer.appendChild(firstButton);
 
 const inputElement = document.createElement("input");
-inputElement.setAttribute("type", "url");
-inputElement.setAttribute("name", "url");
-inputElement.setAttribute("id", "url");
+
 inputElement.setAttribute("placeholder", "www.example.com");
-inputElement.setAttribute("size", "0");
 appContainer.appendChild(inputElement);
 
 const secondButton = document.createElement("button");
 secondButton.textContent = "Переадресовується на інший сайт";
-secondButton.setAttribute("type", "button");
-
 appContainer.appendChild(secondButton);
 
 let url = "";
-
 const validRedirect = (urlFromInput) => {
   if (urlFromInput.trim() === "") {
-    return alert("Ведіть коректно URL наприклад www.amazon.com");
+    alert("Please enter a valid URL, for example: www.amazon.com");
+    return;
   }
-  !urlFromInput.startsWith("http://") && !urlFromInput.startsWith("https://")
-    ? (url = "http://" + urlFromInput)
-    : (url = urlFromInput);
+
+  const regex = /^(https?:\/\/)/;
+  url = regex.test(urlFromInput) ? urlFromInput : "http://" + urlFromInput;
+  console.log(url, regex.test(urlFromInput));
   return (inputElement.value = url);
 };
 
-firstButton.addEventListener("click", function () {
-  const urlFromInput = inputElement.value;
-  validRedirect(urlFromInput);
+firstButton.addEventListener("click", () => {
+  validRedirect(inputElement.value);
 });
 
-secondButton.addEventListener("click", function () {
+secondButton.addEventListener("click", () => {
+  console.log(url);
   window.open(url, "_blank");
 });
