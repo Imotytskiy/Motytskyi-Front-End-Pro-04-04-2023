39038501966ccf05bc0979bb543d64bfeac8aa8e
Imotytskiy <Imotytskiy@gmail.com>
2023-05-24 13:49:49 +0200

    add css update logic

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-const parentElement = document.querySelector("body");
-parentElement.style.backgroundColor = "#89CFF0";
-parentElement.style.marginTop = "20%";
-parentElement.style.display = "flex";
-parentElement.style.justifyContent = "space-around";
-parentElement.style.alignItems = "center";
+const appContainer = document.getElementById("root");
 
 const firstButton = document.createElement("button");
 firstButton.textContent = "Реалізувати перевірку на http/https";
 firstButton.setAttribute("type", "submit");
-parentElement.appendChild(firstButton);
+appContainer.appendChild(firstButton);
 
 const inputElement = document.createElement("input");
 inputElement.setAttribute("type", "url");
@@ -16,31 +11,25 @@ inputElement.setAttribute("name", "url");
 inputElement.setAttribute("id", "url");
 inputElement.setAttribute("placeholder", "www.example.com");
 inputElement.setAttribute("size", "0");
-parentElement.appendChild(inputElement);
+appContainer.appendChild(inputElement);
 
 const secondButton = document.createElement("button");
 secondButton.textContent = "Переадресовується на інший сайт";
 secondButton.setAttribute("type", "button");
 
-parentElement.appendChild(secondButton);
+appContainer.appendChild(secondButton);
 
 let url = "";
 
-function validRedirect(urlFromInput) {
+const validRedirect = (urlFromInput) => {
   if (urlFromInput.trim() === "") {
-    alert("Ведіть коректно URL наприклад www.amazon.com");
+    return alert("Ведіть коректно URL наприклад www.amazon.com");
   }
-  if (
-    !urlFromInput.startsWith("http://") &&
-    !urlFromInput.startsWith("https://")
-  ) {
-    url = "http://" + urlFromInput;
-  } else {
-    url = urlFromInput;
-  }
-  inputElement.value = url;
-  return url;
-}
+  !urlFromInput.startsWith("http://") && !urlFromInput.startsWith("https://")
+    ? (url = "http://" + urlFromInput)
+    : (url = urlFromInput);
+  return (inputElement.value = url);
+};
 
 firstButton.addEventListener("click", function () {
   const urlFromInput = inputElement.value;
