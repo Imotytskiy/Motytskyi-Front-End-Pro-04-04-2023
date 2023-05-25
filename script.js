f6ea227bcab0eff41d3fa03e7f6d1907a1b4c0ed
Imotytskiy <Imotytskiy@gmail.com>
2023-05-25 11:58:32 +0200

    add  buttons id

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 const appContainer = document.getElementById("root");
 
 const firstButton = document.createElement("button");
+firstButton.id = "checkButton";
 firstButton.textContent = "Реалізувати перевірку на http/https";
 appContainer.appendChild(firstButton);
 
 const inputElement = document.createElement("input");
-
 inputElement.setAttribute("placeholder", "www.example.com");
 appContainer.appendChild(inputElement);
 
 const secondButton = document.createElement("button");
 secondButton.textContent = "Переадресовується на інший сайт";
+secondButton.id = "redirectButton";
 appContainer.appendChild(secondButton);
 
 let url = "";
 const validRedirect = (urlFromInput) => {
+  let regex = /^(https?:\/\/)/;
   if (urlFromInput.trim() === "") {
-    alert("Please enter a valid URL, for example: www.amazon.com");
+    alert(
+      "Будь ласка, введіть дійсну електронну адресу наприклад: www.amazon.com"
+    );
     return;
+  } else {
+    url = regex.test(urlFromInput) ? urlFromInput : "http://" + urlFromInput;
+    return url;
   }
-
-  const regex = /^(https?:\/\/)/;
-  url = regex.test(urlFromInput) ? urlFromInput : "http://" + urlFromInput;
-  console.log(url, regex.test(urlFromInput));
-  return (inputElement.value = url);
 };
 
-firstButton.addEventListener("click", () => {
-  validRedirect(inputElement.value);
-});
-
-secondButton.addEventListener("click", () => {
-  console.log(url);
-  window.open(url, "_blank");
+document.addEventListener("click", (event) => {
+  if (event.target.id === "checkButton") {
+    validRedirect(inputElement.value);
+  } else if (event.target.id === "redirectButton") {
+    window.open(url, "_blank");
+  }
 });
