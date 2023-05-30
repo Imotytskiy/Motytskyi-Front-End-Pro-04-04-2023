5ff04417edb70469e4f4587c38b087ca19bffdae
Imotytskiy <Imotytskiy@gmail.com>
2023-05-30 14:23:14 +0200

    add

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 function chbx(obj) {
-  var that = obj;
-  if (document.getElementById(that.id).checked == true) {
-    document.getElementById("ENG").checked = false;
-    document.getElementById("UKR").checked = false;
-    document.getElementById("POL").checked = false;
-    document.getElementById(that.id).checked = true;
-  }
+  let id = obj.id;
+  document.getElementById("ENG").checked = false;
+  document.getElementById("UKR").checked = false;
+  document.getElementById("POL").checked = false;
+  setTimeout(() => {
+    document.getElementById(id).checked = true;
+  }, 0);
 }
 
-addEventListener;
+document.getElementById("ENG").addEventListener("click", function () {
+  chbx(this);
+});
+
+document.getElementById("UKR").addEventListener("click", function () {
+  chbx(this);
+});
+
+document.getElementById("POL").addEventListener("click", function () {
+  chbx(this);
+});
