fd85726f0b05bd1ed2f8a2b5c583507c31b0968f
Imotytskiy <Imotytskiy@gmail.com>
2023-05-31 23:38:44 +0200

    add finish

--- script.js
+++ script.js
@@ -1,57 +1,20 @@
-const formTitles = [
-  "Ім'я",
-  "Прізвище",
-  "Дата народження",
-  "Стать",
-  "Місто",
-  "Адреса",
-  "Мови, якими володіє",
-];
-console.log(formTitles.length);
 const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const formContainer = document.getElementById("formContainer");
-  formContainer.style.display = "none";
+  document.getElementById("formContainer").style.display = "none";
+  document.getElementById("tableContainer").style.display = "block";
 
-  const tableContainer = document.getElementById("tableContainer");
-  tableContainer.style.display = "block";
-
-  const formData = new FormData(form);
-  let i = 1;
-  let str = "";
-  formData.forEach((value) => {
-    const formKey = document.getElementById(i.toString() + "a");
-    formKey.innerHTML = formTitles[i - 1];
-    const formValue = document.getElementById(i.toString() + "b");
-    if (i <= formTitles.length - 1) {
-      formValue.innerHTML = value;
-      i++;
-    } else {
-      str = str + " " + value;
-    }
-    document.getElementById("7b").innerHTML = str;
-  });
+  document.getElementById("name").innerHTML = form["first-name"].value;
+  document.getElementById("surname").innerHTML = form["last-name"].value;
+  document.getElementById("birth").innerHTML = form["birth"].value;
+  document.getElementById("gender").innerHTML = form["gender"].value;
+  document.getElementById("cityValue").innerHTML = form["city"].value;
+  document.getElementById("adress").innerHTML = form["adress"].value;
+  let lang = "";
+  lang = form["langEng"].checked ? form["langEng"].value + "<br>" : "";
+  lang += form["langUkr"].checked ? form["langUkr"].value + "<br>" : "";
+  lang += form["langPol"].checked ? form["langPol"].value + "<br>" : "";
+  document.getElementById("lang").innerHTML = lang;
 });
-
-// function chbx(obj) {                                                              // жалко удалять задание неправильно прочитал
-//   document.getElementById("ENG").checked = false;
-//   document.getElementById("UKR").checked = false;
-//   document.getElementById("POL").checked = false;
-//   document.getElementById(obj.id).checked = true;
-// }
-
-// document.getElementsByName("lang");
-// document.getElementById("ENG").addEventListener("click", function () {
-//   chbx(this);
-// });
-
-// document.getElementById("UKR").addEventListener("click", function () {
-//   chbx(this);
-// });
-
-// document.getElementById("POL").addEventListener("click", function () {
-//   chbx(this);
-// });
