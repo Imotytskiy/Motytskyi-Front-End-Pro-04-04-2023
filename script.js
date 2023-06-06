1e3e3be9c87606bb02727cf4e5b6dbaf3f54b73a
Imotytskiy <Imotytskiy@gmail.com>
2023-06-06 17:22:35 +0200

    add

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function takeInfo() {
     fetch(`https://jsonplaceholder.typicode.com/todos/${number}`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        console.log(data, typeof data);
       })
       .catch((error) => {
         console.error(error);
