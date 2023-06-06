8b4556426eefb5e21e6ef334cc3f871e4c1352f9
Imotytskiy <Imotytskiy@gmail.com>
2023-06-06 15:32:25 +0200

    add

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function takeInfo() {}
+function takeInfo() {
+  var form = document.getElementById("myForm");
 
-function searchPost() {}
+  document
+    .getElementsByTagName("button")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      console.log(event);
+    });
+}
+takeInfo();
+// function searchPost() {}
