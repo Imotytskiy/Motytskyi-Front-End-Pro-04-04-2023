d59d1bd857aeee10bdf02f38bb62d1f955a79d86
Imotytskiy <Imotytskiy@gmail.com>
2023-06-12 12:32:00 +0200

    add errror

--- script.js
+++ script.js
@@ -1,59 +1,44 @@
-let dataJson = null;
-let postId = null;
-let comments = null;
-
 function takePost() {
   const form = document.getElementById("form");
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
-    postId = form.elements.post.value;
+    const postId = form.elements.post.value;
+    takePostComments(postId);
 
-    fetch(`https://jsonplaceholder.typicode.com/todos/${postId}`)
-      .then((response) => response.json())
-      .then((data) => {
-        document.getElementById("choose-post").style.display = "none";
-        document.getElementById("post-button").style.display = "block";
-        show(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+      .then((res) => (res.ok ? res : Promise.reject(res)))
+      .then((res) => res.json())
+      .then((data) => showPost(data))
+      .catch((error) => console.error(error));
   });
 }
 
 takePost();
 
-function takePostComments() {
+function takePostComments(postId) {
   const form = document.getElementById("comment-button");
 
   form.addEventListener("click", function (event) {
     fetch(`https://jsonplaceholder.typicode.com/post/${postId}/comments`)
-      .then((response) => response.json())
-      .then((data) => {
-        show(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+      .then((res) => (res.ok ? res : Promise.reject(res)))
+      .then((res) => res.json())
+      .then((data) => showComments(data))
+      .catch((error) => console.error(error));
   });
 }
 
-takePostComments();
+function showPost(post) {
+  console.log(post, post.title);
+  const postContainer = document.getElementById("seepost");
+  postContainer.innerText = `userId: ${post.userId} \n id: ${post.id} \n title: ${post.title} \n  body: ${post.body}`;
+}
 
-function show(info) {
-  document.getElementById("write-obj").style.display = "block";
-  console.log(info, info.title);
-  const post = document.getElementById("seepost");
-  console.log(info, typeof info);
-  if (comments) {
-    let str = "";
-    for (let i = 0; i < info.length; i++) {
-      str += `${info[i].name} <br>`;
-    }
-    post.innerHTML = str;
-  } else {
-    post.innerText =
-      info.userId + "\n" + info.id + "\n" + info.title + "\n" + info.body;
-  }
+function showComments(comments) {
+  let str = "";
+  for (let i = 0; i < comments.length; i++)
+    str += `Ім'я: ${comments[i].name} <br> E-mail: ${
+      comments[i].email
+    } <br> КОМЕНТАР №${i + 1}: ${comments[i].body} <br><br>`;
+  document.getElementById("seepost").innerHTML = str;
 }
