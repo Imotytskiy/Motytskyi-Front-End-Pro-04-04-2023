47cfe08e175dc6e3c202aa9f395152a279cefd2a
Imotytskiy <Imotytskiy@gmail.com>
2023-06-13 12:30:30 +0200

    add strong

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ function takePost() {
   form.addEventListener("submit", function (event) {
     event.preventDefault();
     const postId = form.elements.post.value;
-    if (postId > 100 || postId < 1) {
-      const errorMsg = "ID посту повинен бути між 1 та 100";
-      console.error(errorMsg);
-      postContainer.innerText = errorMsg;
-      return;
-    }
+    // if (postId > 100 || postId < 1) {
+    //   const errorMsg = "ID посту повинен бути зазначене від 1 до 100";
+    //   console.error(errorMsg);
+    //   postContainer.innerText = errorMsg;
+    //   return;
+    // }
     takePostComments(postId);
 
     fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
       .then((res) => {
         if (!res.ok) {
+          // 200-299
           if (res.status === 404) {
             const errorMsg = "Пост не знайдено";
             console.error(errorMsg);
@@ -71,7 +72,14 @@ function takePostComments(postId) {
 function showPost(post) {
   console.log(post, post.title);
   const postContainer = document.getElementById("seepost");
-  postContainer.innerText = `userId: ${post.userId} \n id: ${post.id} \n title: ${post.title} \n  body: ${post.body}`;
+  postContainer.style.display = "block";
+  postContainer.innerHTML = `
+    <p><strong>userId:</strong> ${post.userId}</p>
+    <p><strong>id:</strong> ${post.id}</p>
+    <p><strong>title:</strong> ${post.title}</p>
+    <p><strong>body:</strong> ${post.body}</p>
+  `;
+  document.getElementById("comment-button").style.display = "block";
 }
 
 function showComments(comments) {
