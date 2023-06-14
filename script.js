a37e19b7f6b25740f3b323fc1d79e89161d2fd9f
Imotytskiy <Imotytskiy@gmail.com>
2023-06-14 15:43:56 +0200

    add https

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let weather = new XMLHttpRequest();
 weather.open(
   "GET",
-  "http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19",
+  "https://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19",
   true
 );
 weather.onreadystatechange = function () {
