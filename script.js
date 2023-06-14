1ea799960adb90b0415452fbfa5f7c37de47f8d6
Imotytskiy <Imotytskiy@gmail.com>
2023-06-14 13:36:13 +0200

    add швидкість

--- script.js
+++ script.js
@@ -9,21 +9,20 @@ weather.onreadystatechange = function () {
     if (weather.status === 200) {
       let readyWeather = JSON.parse(weather.responseText);
       console.log(readyWeather);
+      document.getElementById("city").innerText = readyWeather.name;
+      document.getElementById("temp").innerText = readyWeather.main.temp;
+      document.getElementById("pressure").innerText =
+        readyWeather.main.pressure;
+      document.getElementById("description").innerText =
+        readyWeather.weather[0].description;
+      document.getElementById("humidity").innerText =
+        readyWeather.main.humidity;
+      document.getElementById("wind-speed").innerText = readyWeather.wind.speed;
+      document.getElementById("deg").innerText = readyWeather.wind.deg;
     } else {
       console.error("Error: " + weather.status);
     }
   }
 };
 weather.send();
-
-window.onload = function () {
-  document.getElementById("city").innerText = readyWeather.name;
-  document.getElementById("temp").innerText = readyWeather.main.temp;
-  document.getElementById("pressure").innerText = readyWeather.main.humidity;
-  document.getElementById("description").innerText = readyWeather.description;
-  document.getElementById("humidity").innerText = readyWeather.humidity;
-  document.getElementById("speed").innerText = readyWeather.wind.speed;
-  document.getElementById("dec").innerText = readyWeather.wind.dec;
-  document.getElementById("icon").src =
-    "http://openweathermap.org/img/w/10d.png";
-};
+document.getElementById("icon").src = "http://openweathermap.org/img/w/10d.png";
