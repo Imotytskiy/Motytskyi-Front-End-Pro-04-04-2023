1b0fa78df0114b53ddf473c4c25589940c5ebb37
Imotytskiy <Imotytskiy@gmail.com>
2023-07-03 00:51:49 +0200

    add comment reset because do not work

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,12 @@ global.app = {
 
 // import tasks
 import { copy } from "./gulp/tasks/copy.js";
-import { reset } from "./gulp/tasks/reset.js";
+// import { reset } from "./gulp/tasks/reset.js";
 
 function watcher() {
   gulp.watch(path.watch.files, copy);
 }
 
-const dev = gulp.series(reset, copy, watcher);
+const dev = gulp.series(copy, watcher); // reset
 
 gulp.task("default", dev);
