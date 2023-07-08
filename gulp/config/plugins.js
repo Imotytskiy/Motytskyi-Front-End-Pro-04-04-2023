656b913bfdde62e05f88f0d71216c66b40f2679e
Imotytskiy <Imotytskiy@gmail.com>
2023-07-08 02:36:10 +0200

    add

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,6 +1,7 @@
 import * as browserSync from "browser-sync";
 import plumber from "gulp-plumber";
 import concat from "gulp-concat";
+import ifPlugin from "gulp-if";
 // import coffee from "gulp-coffee";
 // import notify from "gulp-notify";
 
@@ -8,6 +9,7 @@ export const plugins = {
   browserSync: browserSync.create(),
   plumber: plumber,
   concat: concat,
+  if: ifPlugin,
   // coffee: coffee,
   // notify: notify,
 };
