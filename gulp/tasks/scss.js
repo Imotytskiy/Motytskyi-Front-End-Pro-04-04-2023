c83ee071ca4ee0e790c1f48a59b9f98e4e688e59
Imotytskiy <Imotytskiy@gmail.com>
2023-07-09 02:20:14 +0200

    add more func for build

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -10,14 +10,22 @@ export const scss = () => {
     .pipe(app.plugins.concat("styles.scss"))
     .pipe(app.plugins.plumber())
     .pipe(
-      autoprefixer({
-        grid: true,
-        overrideBrowserslist: ["last 3 versions"],
-        cascade: true,
-      })
+      app.plugins.if(
+        app.isBuild,
+        autoprefixer({
+          grid: true,
+          overrideBrowserslist: ["last 3 versions"],
+          cascade: true,
+        })
+      )
     )
-    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
-    .pipe(cleanCss())
+    .pipe(
+      sass({ outputStyle: app.isBuild ? "compressed" : "expanded" }).on(
+        "error",
+        sass.logError
+      )
+    )
+    .pipe(app.plugins.if(app.isBuild, cleanCss()))
     .pipe(app.gulp.dest(app.path.build.scss))
     .pipe(app.plugins.browserSync.stream());
 };
