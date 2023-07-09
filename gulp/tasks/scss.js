aa9dd9d09356a0fe115e2cc4bf54ff908d77bc57
Imotytskiy <Imotytskiy@gmail.com>
2023-07-09 02:07:03 +0200

    add autoprefixer

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,4 +1,5 @@
-// import concat from "gulp-concat";
+import cleanCss from "gulp-clean-css";
+import autoprefixer from "gulp-autoprefixer";
 import dartSass from "sass";
 import gulpSass from "gulp-sass";
 const sass = gulpSass(dartSass);
@@ -8,7 +9,15 @@ export const scss = () => {
     .src(app.path.src.scss)
     .pipe(app.plugins.concat("styles.scss"))
     .pipe(app.plugins.plumber())
+    .pipe(
+      autoprefixer({
+        grid: true,
+        overrideBrowserslist: ["last 3 versions"],
+        cascade: true,
+      })
+    )
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(cleanCss())
     .pipe(app.gulp.dest(app.path.build.scss))
     .pipe(app.plugins.browserSync.stream());
 };
