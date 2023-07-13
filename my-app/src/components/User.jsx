d5e3093d1308402f71b525ca21e182a39d2f305b
Imotytskiy <Imotytskiy@gmail.com>
2023-07-13 19:33:21 +0300

    add hook

--- my-app/src/components/User.jsx
+++ my-app/src/components/User.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 const User = ({name}) => {
-    const state = useState(0);
-    console.log(state);
-    let counter = 0;
+    setCounter(counter+1)
+    const [counter, setCounter] = useState(0);
+
     const clickHandler = (event) => {
     counter++;}
     return( 
@@ -13,7 +13,7 @@ const User = ({name}) => {
             <div className="user_name">{name}</div>
             <div className="counter">{counter}</div>
             
-            <button onClick={clickHandler}    >inc +</button>
+            <button onClick={clickHandler}  >inc +</button>
             <button>dec -</button>
         </div>
     )
