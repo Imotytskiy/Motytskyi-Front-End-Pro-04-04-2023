dfc198ec9d7dcfd274bf70ae2c32e965b67a285c
Imotytskiy <Imotytskiy@gmail.com>
2023-07-13 13:31:58 +0300

    add

--- gulp/tasks/html.js
+++
@@ -1,8 +0,0 @@
-import fileInclude from "gulp-file-include";
-export const html = () => {
-  return app.gulp
-    .src(app.path.src.html)
-    .pipe(app.plugins.plumber())
-    .pipe(fileInclude())
-    .pipe(app.gulp.dest(app.path.build.html));
-};
